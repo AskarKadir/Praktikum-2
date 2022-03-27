9d5a0edfa03d657593c4706858451ea61a02a2f9
AskarKadir <askarkadir07@gmail.com>
2022-03-27 23:41:15 +0700

    Exe1 by Askar Kadir

--- app/src/main/java/com/example/praktikum2/MainActivity.java
+++ app/src/main/java/com/example/praktikum2/MainActivity.java
@@ -1,20 +1,19 @@
 package com.example.praktikum2;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     //deklarasi variabel
     Button Blogin;
+    TextView Tregis;
 
     EditText emailinput, passwordinput;
 
@@ -22,22 +21,22 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        //kondisi
-        if (item.getItemId() == R.id.mnDaftar){
-            Intent i = new Intent(getApplicationContext(),DaftarAct.class);
-            startActivity(i);
-        }
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+//        //kondisi
+//        if (item.getItemId() == R.id.mnDaftar){
+//            Intent i = new Intent(getApplicationContext(),DaftarAct.class);
+//            startActivity(i);
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        //method untuk menampilkan menu
-        getMenuInflater().inflate(R.menu.menu,menu);
-        return super.onCreateOptionsMenu(menu);
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        //method untuk menampilkan menu
+//        getMenuInflater().inflate(R.menu.menu,menu);
+//        return super.onCreateOptionsMenu(menu);
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,10 +44,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //menghubungkan button pada layout
         Blogin = findViewById(R.id.buttonsign);
+        Tregis = findViewById(R.id.register);
 
         emailinput = findViewById(R.id.email);
 
         passwordinput = findViewById(R.id.passw);
+
+        Tregis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                menampilkan halaman daftar
+                Intent dft = new Intent(getApplicationContext(),daftar.class);
+                startActivity(dft);
+
+            }
+        });
+
 
         //membuat fungsi onclick pada button
         Blogin.setOnClickListener(new View.OnClickListener() {
@@ -68,8 +79,8 @@ public class MainActivity extends AppCompatActivity {
                       b.putString("b", passwd.trim());
 
                       //membuat objek untuk pindah halaman sehingga dapat memanggail java class yang baru
-                      Intent i = new Intent(getApplicationContext(), MainActivity2.class);
-                      //memasukkan bundle ke daslam intent
+                      Intent i = new Intent(getApplicationContext(), haltodo.class);
+                      //memasukkan bundle ke dalam intent
                       i.putExtras(b);
                       //berpindah ke halaman lain
                       startActivity(i);
@@ -78,22 +89,34 @@ public class MainActivity extends AppCompatActivity {
                               "Anda Berhasil Login", Toast.LENGTH_LONG);
                       t.show();
 
-                  } else if (user.equals("admin@corp.id") && !passwd.equals("SAdmin")) {
-                      Toast t = Toast.makeText(getApplicationContext(),
+                  } else if (user.equals("1") && !passwd.equals("1")) {
+                      /*Toast t = Toast.makeText(getApplicationContext(),
                               "Password Anda Salah", Toast.LENGTH_LONG);
-                      t.show();
-                  } else if (!user.equals("admin@copr.id") && passwd.equals("SAdmin")) {
-                      Toast t = Toast.makeText(getApplicationContext(),
+                      t.show();*/
+                      showErrorPassword(passwordinput,"Password Anda Salah");
+
+                  } else if (!user.equals("1") && passwd.equals("1")) {
+                      /*Toast t = Toast.makeText(getApplicationContext(),
                               "Email Anda salah", Toast.LENGTH_LONG);
-                      t.show();
+                      t.show();*/
+                      showErrorEmail(emailinput,"Email Anda Salah");
                   } else {
-                      Toast t = Toast.makeText(getApplicationContext(),
+                      /*Toast t = Toast.makeText(getApplicationContext(),
                               "Email dan Password Anda Salah", Toast.LENGTH_LONG);
-                      t.show();
+                      t.show();*/
+
+                      showErrorEmail(emailinput,"Email Anda Salah");
+                      showErrorPassword(passwordinput,"Password Anda Salah");
 
                   }
               }
-          }
+              private void showErrorEmail(EditText email, String m) {
+                  email.setError(m);
+              }
+              private void showErrorPassword(EditText cpassword, String m) {
+                  cpassword.setError(m);
+              }
+        }
         );
     }
 }
